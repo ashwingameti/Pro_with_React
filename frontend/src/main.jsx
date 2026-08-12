3ac030c2118ed1a5f530e1896974abd5f13ea4f0
ashwingameti <gashwin009@gmail.com.com>
2026-08-12 15:15:37 +0530

    Update readmi file

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
-
 import App from "./App";
 import "./index.css";
 
@@ -13,4 +12,4 @@ ReactDOM.createRoot(
       <App />
     </BrowserRouter>
   </React.StrictMode>
-);
+);  
